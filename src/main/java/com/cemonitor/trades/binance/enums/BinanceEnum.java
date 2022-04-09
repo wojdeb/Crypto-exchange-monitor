8ddebec47a5923f3ee7d8195acbf83de170cda45
96peterharris <96peterharris@gmail.com>
2022-04-09 13:06:06 +0200

    Binance services #1

---
+++ src/main/java/com/cemonitor/trades/binance/enums/BinanceEnum.java
@@ -0,0 +1,24 @@
+package com.cemonitor.trades.binance.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ *
+ * @author psobieraj
+ * @date 2022-04-09 11:00
+ **/
+@Getter
+public enum BinanceEnum
+{
+    GET_PRICES("https://api.binance.com/api/v3/ticker/price");
+
+    private final String value;
+
+    BinanceEnum(String value)
+    {
+        this.value = value;
+    }
+}
