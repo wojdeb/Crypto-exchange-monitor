b3f3e951d94a95d0d67d944e72f907762840901c
96peterharris <96peterharris@gmail.com>
2022-04-09 10:00:18 +0200

    Binance services #1

--- src/main/java/com/cemonitor/DemoApplication.java
+++ src/main/java/com/cemonitor/DemoApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
